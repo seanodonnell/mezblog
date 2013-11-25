82fd04e075d68f1218270941f676e493b7604c89
Sean O'Donnell <sean@odonnell.nu>
2013-11-25 14:49:52 +0000

    fixing comment counts

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@
 #turning this off to allow pygments to work
 RICHTEXT_FILTER_LEVEL = 3
 
+COMMENTS_UNAPPROVED_VISIBLE = False
+COMMENTS_REMOVED_VISIBLE = False
 
 MCE_DEFAULT_LEXERS = ['Python', 'JavaScript']
 MCE_DEFAULT_STYLE = 'pastie' 
