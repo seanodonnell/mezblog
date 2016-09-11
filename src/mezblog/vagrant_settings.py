9234ef4240b9c8003d18cca2096cda8f83cc008c
Sean O'Donnell <sean@odonnell.nu>
2016-09-12 00:50:52 +0100

    added vagrant settings file

---
+++ src/mezblog/vagrant_settings.py
@@ -0,0 +1,17 @@
+DEBUG = False
+
+SECRET_KEY = "1aa1bb74-e7uu-4a86-921e-1b888fffffffffffffff-ffff-1111-2222-bbbbbbbbbbbbbbbbbbbb-1111-1111-1111-111111111111"
+NEVERCACHE_KEY = "22222222-2222-2222-2222-22222222222222222222-2222-2222-2222-22222222222222222222-2222-2222-2222-222222222222"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mezblog",
+        "USER": "mezblog",
+        "PASSWORD": "Phah3hai",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
