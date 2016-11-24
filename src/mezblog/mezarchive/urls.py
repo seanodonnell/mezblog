c0a5e6ababe51930d7cac0d72c34aa5158bf4ed4
Sean O'Donnell <sean@odonnell.nu>
2016-11-24 21:18:10 +0000

    fixing archive urls

--- src/mezblog/mezarchive/urls.py
+++ src/mezblog/mezarchive/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, patterns
+from mezarchive import views
 
-urlpatterns = patterns("",
-url("^archive/$" ,
-        "mezarchive.views.blog_post_archive", name="blog_archive"
-    ),
-)
+
+urlpatterns = [
+    url("^archive/$", views.blog_post_archive, name="blog_archive"),
+]
