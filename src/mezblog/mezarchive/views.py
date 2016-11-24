3004b95406c9784b4c3ca67729cd5c331e89bbb9
Sean O'Donnell <sean@odonnell.nu>
2016-11-24 21:28:03 +0000

    pep8 cleanup

--- src/mezblog/mezarchive/views.py
+++ src/mezblog/mezarchive/views.py
@@ -2,6 +2,7 @@ from mezzanine.conf import settings
 from mezzanine.blog.models import BlogPost
 from mezzanine.utils.views import render
 
+
 def blog_post_archive(request, template="blog/blog_post_list.html"):
     """
     Display all blog posts
@@ -9,7 +10,7 @@ def blog_post_archive(request, template="blog/blog_post_list.html"):
     settings.use_editable()
 
     blog_posts = BlogPost.objects.published(for_user=request.user)
-    
+
     prefetch = ("categories", "keywords__keyword")
     blog_posts = blog_posts.select_related("user").prefetch_related(*prefetch)
 
@@ -17,5 +18,5 @@ def blog_post_archive(request, template="blog/blog_post_list.html"):
         def __init__(self, object_list):
             self.object_list = object_list
 
-    context = {"blog_posts": ObjectList(blog_posts) }
+    context = {"blog_posts": ObjectList(blog_posts)}
     return render(request, "blog/blog_post_list.html", context)
